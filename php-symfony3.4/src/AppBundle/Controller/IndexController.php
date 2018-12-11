3084452edf7adb580658991faa190e2574b66b7e
Steffen Beisenherz <steffen.beisenherz@gmail.com>
2018-12-11 19:39:58 +0100

    Adding Symfony3.4

---
+++ php-symfony3.4/src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('index/index.html.twig');
+    }
+}
